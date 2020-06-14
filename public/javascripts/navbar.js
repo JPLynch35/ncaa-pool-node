451d2010ee8ea542b2886d5f8d0c7b2a4a426f7a
JP Lynch <JPLynch35@gmail.com>
2020-06-14 17:10:48 -0400

    Update site UI further with marble

--- public/javascripts/navbar.js
+++ public/javascripts/navbar.js
@@ -1,5 +1,5 @@
 // highlight current active page in navbar
 $(document).ready(function() {
   var pathname = window.location.pathname;
-  $('nav li > a[href="'+pathname+'"]').parent().addClass('active');
+  $('nav li > a[href="'+pathname+'"]').parent().addClass('fh5co-active');
 })
