14a2d4482a938ffa275f87f5a3e8fbda8ddb42e9
JP Lynch <JPLynch35@gmail.com>
2020-06-22 17:18:52 -0400

    Add additional seasons to seed file

--- data/seeds/addSeasons.js
+++ data/seeds/addSeasons.js
@@ -3,7 +3,9 @@ exports.seed = knex => {
     .then(() => {
       return knex('seasons').insert([
         {id: 1, year: 2020, spending_cap: 7500000, start_date: '2020-08-15', end_date: '2021-01-15', current: true},
-        {id: 2, year: 2021, spending_cap: 7500000, start_date: '2021-08-15', end_date: '2022-01-15', current: false}
+        {id: 2, year: 2021, spending_cap: 7500000, start_date: '2021-08-15', end_date: '2022-01-15', current: false},
+        {id: 3, year: 2022, spending_cap: 7500000, start_date: '2022-08-15', end_date: '2023-01-15', current: false},
+        {id: 4, year: 2023, spending_cap: 7500000, start_date: '2023-08-15', end_date: '2024-01-15', current: false}
       ]);
     });
 };
